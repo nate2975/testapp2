86ebce86b048b5ecae7ec5a2375f22a40d98d045
Nate <nateb2975@gmail.com>
2017-07-25 23:58:26 +0000

    fixed up article sitch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minmium: 3, maximum: 50 }
-    validates :description, presence: true, length: { minmium: 10, maximum: 300 }  
+  validates :title, presence: true, length: {minmium: 3, maximum: 50}
+    validates :description, presence: true, length: {minmium: 10, maximum: 300}  
 end
