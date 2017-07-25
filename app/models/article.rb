a78fde0c18721411e1f940a54a6a802ef724968d
Nate <nateb2975@gmail.com>
2017-07-25 21:40:33 +0000

    articles

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minmium: 3, maximum: 50 }
+    validates :description, presence: true, length: { minmium: 10, maximum: 300 }  
+end
