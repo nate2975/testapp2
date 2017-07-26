4892986c06f6a9ce4c743e6aa2c335558c2cac8e
Nate <nateb2975@gmail.com>
2017-07-26 03:12:42 +0000

    added gem bootstrap, and css.scss, and js (not sure)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,19 @@ class ArticlesController < ApplicationController
    @article = Article.find(params[:id])
   end
   
+  def destroy
+   @article = Article.find(params[:id])
+   @article.destroy
+   flash[:notice] = "Article was successfully deleted"
+   redirect_to articles_path
+  end
    private 
+   def set_article
+
+@article = Article.find(params[:id])
+
+end
+
    def article_params
      params.require(:article).permit(:title, :description)
    end
