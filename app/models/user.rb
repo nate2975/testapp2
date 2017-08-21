e54da5d6ec5bd1c3665d9c8f2c9dbbef239ad3cd
Nate <nateb2975@gmail.com>
2017-08-21 19:15:29 +0000

    added users etc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-    
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }
+
+validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
