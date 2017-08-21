269c5282fdd2a54653e2df8ec267de61d7c7b6c7
Nate <nateb2975@gmail.com>
2017-08-21 21:31:28 +0000

    updated controller...debugger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807185148) do
+ActiveRecord::Schema.define(version: 20170821195555) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170807185148) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "img"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
