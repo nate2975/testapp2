18949ffc1761c946ff8c71dd6ef8cd6ba7c1c33c
Nate <nateb2975@gmail.com>
2017-08-22 21:30:10 +0000

    add password digest..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }
 
 validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807185148) do
+ActiveRecord::Schema.define(version: 20170822211946) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170807185148) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "img"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -33,8 +34,9 @@ ActiveRecord::Schema.define(version: 20170807185148) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
